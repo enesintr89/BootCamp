856db90d59951074531b7efec3c22fc39ae55448
enes <enes@DESKTOP-501Q4TR>
2022-04-28 23:25:06 +0300

    Add project files.

---
+++ Collection/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string >isimler2=new List<string> { "Engin","Ahmet","Orhan"};
+
+Console.WriteLine(isimler2[0]);
+isimler2.Add("İlker");
+Console.WriteLine(isimler2[3]);
+
+foreach (var item in isimler2)
+{
+
+    Console.WriteLine(item);
+}
+
+
+
+
+
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsIntro;
+
+Console.WriteLine("Hello, World!");
+
+MyList<string> isimler = new MyList<string>();
+
+isimler.Add("Enes");
+
